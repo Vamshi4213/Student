8dd2d4534ed219a307ea6d49aa37643ba1df965e
Vamshi4213 <103871036+Vamshi4213@users.noreply.github.com>
2025-12-26 08:37:48 +0530

    Updates

--- Student.java
+++ Student.java
@@ -9,5 +9,6 @@ public class Student{
         System.out.println("My Roll Number is: " + rollno);
         System.out.println("=======================");
         System.out.println("GOOD Evening");
+        System.out.println("GOOD Evening");
     }
 }
